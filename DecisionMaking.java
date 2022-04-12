40d71e54d933a5273e4b078e1b114abf23f840b3
BungaPricilia <102948759+BungaPricilia@users.noreply.github.com>
2022-04-13 00:55:02 +0700

    OOP 2

---
+++ DecisionMaking.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package decisionmaking;
+
+
+public class DecisionMaking {
+
+    public static void main(String[] args) {
+      
+        int score = 95;
+        
+        if(score >= 90){
+            System.out.println("you scored a " + score + "on the exam.");
+            System.out.println("Great job on the exam!");
+            System.out.println("I like apples.");
+        }
+        
+        else{
+             System.out.println("you scored a " + score + "on the exam.");
+             System.out.println("You did poorly on the exam!");
+             System.out.println("I like oranges.");
+        }
+    }
+    
+}
